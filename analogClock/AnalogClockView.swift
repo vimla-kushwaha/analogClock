04f613a0d294f6982982b8fa3f6beedf10b619ef
vimla kushwaha <vimlakushwaha01@gmail.com>
2024-05-14 22:38:19 +0530

    almost complleted

--- analogClock/AnalogClockView.swift
+++ analogClock/AnalogClockView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct AnalogClockView: View {
-    typealias AnalogClockCallBack = (Data)->Void
+    typealias AnalogClockCallBack = (Date)->Void
     
     @Binding var foregroundColor: Color
     @State private var currentTime : Date = Date.now
@@ -34,15 +34,96 @@ struct AnalogClockView: View {
             let hour = Double(components.hour ?? 0)
             let minutes = Double(components.minute ?? 0)
             let second = Double(components.second ?? 0)
-            
+            //create border
             Circle()
                 .foregroundColor(foregroundColor)
-            
+            //clock dial
             Circle()
                 .foregroundColor(.white)
                 .padding(borderWidth)
             
+            Path{path in
+                for index in 0..<60{
+                    let radian = Angle(degrees: Double(index)*6-90).radians
+                    
+                    let lineHeight:Double = index%5==0 ? 25:10
+                    let x1 = centerX + innerRadius * cos(radian)
+                    let y1 = centerY + innerRadius * sin(radian)
+                    
+                    let x2 = centerX + (innerRadius - lineHeight) * cos(radian)
+                    let y2 = centerY + (innerRadius - lineHeight) * sin(radian)
+                    
+                    path.move(to: .init(x:x1, y:y1))
+                    path.addLine(to: .init(x:x2, y:y2))
+                }
+                
+            }
+            .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round))
+            .foregroundColor(foregroundColor)
             
+            //Drawing seconds hands
+            
+            Path{
+                path in
+                path.move(to: center)
+                
+                let height = innerRadius - 20
+                let radian = Angle(degrees: second * 6 - 90).radians
+                
+                let x = centerX + height * cos(radian)
+                let y = centerY + height * sin(radian)
+                
+                path.addLine(to: CGPoint(x:x, y:y))
+            }
+            .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round))
+            .foregroundColor(foregroundColor)
+            
+            
+            //Drawing minutes hands
+            
+            Path{
+                path in
+                path.move(to: center)
+                
+                let height = innerRadius * 0.7
+                let radian = Angle(degrees: minutes * 6 - 90).radians
+                
+                let x = centerX + height * cos(radian)
+                let y = centerY + height * sin(radian)
+                
+                path.addLine(to: CGPoint(x:x, y:y))
+            }
+            .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round))
+            .foregroundColor(foregroundColor)
+            
+            
+            //Drawing hour hands
+            
+            Path{
+                path in
+                path.move(to: center)
+                
+                let height = innerRadius * 0.45
+                let radian = Angle(degrees: hour * 6 - 90).radians
+                
+                let x = centerX + height * cos(radian)
+                let y = centerY + height * sin(radian)
+                
+                path.addLine(to: CGPoint(x:x, y:y))
+            }
+            .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
+            .foregroundColor(foregroundColor)
+            
+            Circle()
+                .frame(width: 10, height: 10)
+                .foregroundColor(foregroundColor)
+                .position(center)
+        }
+        .aspectRatio(1, contentMode: .fit)
+        .onReceive(timer){
+            time in
+            currentTime = time
+            onUpdateTime?(time)
         }
     }
 }
