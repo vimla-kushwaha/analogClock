85b6196358b00caab3cb03b555f9dacff432641d
vimla kushwaha <vimlakushwaha01@gmail.com>
2024-05-14 21:15:38 +0530

    Initial Commit

---
+++ analogClock/analogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  analogClockApp.swift
+//  analogClock
+//
+//  Created by vimla kushwaha on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct analogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
